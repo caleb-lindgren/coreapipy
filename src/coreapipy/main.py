adb0055ca62702c4f8ea5e42c1f55690874062cd
Caleb Lindgren <calebmlindgren@gmail.com>
2026-01-13 12:03:37 -0500

    Allow specifying different server, still need to fix authentication though

--- src/coreapipy/main.py
+++ src/coreapipy/main.py
@@ -2,8 +2,6 @@ import io
 import polars as pl
 import requests
 
-server = "bison.med.harvard.edu"
-base_url = f"https://{server}/gfy/www/modules/api/v1"
 
 with open(".username", "r") as username_file:
 	username = username_file.readline()
@@ -14,24 +12,31 @@ with open(".api_key", "r") as api_key_file:
 session = requests.Session()
 session.auth = (username, api_key)
 
-def get_search_info(search_id):
+def get_search_info(search_id, server):
 
+	base_url = f"https://{server}/gfy/www/modules/api/v1"
 	url = f"{base_url}/search/{search_id}"
 	resp = session.get(url)
 	search_info = resp.json()
 	return search_info
 
-def get_searches(search_ids=None, list_to_str=True):
+def get_searches(
+	search_ids=None,
+	list_to_str=True,
+	server="bison.med.harvard.edu",
+):
 
-	if search_ids is None:
+	if search_ids is None:		
+		base_url = f"https://{server}/gfy/www/modules/api/v1"
 		url = f"{base_url}/search"
 		resp = session.get(url)
 		search_ids = [search["search_id"] for search in resp.json()]
 
 	info_list = [
-		get_search_info(search_id)
+		get_search_info(search_id, server=server)
 		for search_id in search_ids
 	]
+	print(info_list)
 	df = pl.DataFrame(info_list)
 
 	if list_to_str:
@@ -41,10 +46,16 @@ def get_searches(search_ids=None, list_to_str=True):
 
 	return df
 
-def get_saved_sets_stats(search_ids, filter_nulls=False, filter_errors=False):
+def get_saved_sets_stats(
+	search_ids,
+	filter_nulls=False,
+	filter_errors=False,
+	server="bison.med.harvard.edu",
+):
 
 	stats_list = []
 	for search_id in search_ids:
+		base_url = f"https://{server}/gfy/www/modules/api/v1"
 		url = f"{base_url}/saved_sets_stats/{search_id}"
 		resp = session.get(url)
 		stats_list.extend(resp.json())
@@ -66,8 +77,10 @@ def get_peptide_view(
 	columns=None,
 	lda=None,
 	schema=None,
+	server="bison.med.harvard.edu",
 ):
 
+	base_url = f"https://{server}/gfy/www/modules/api/v1"
 	url = f"{base_url}/peptide_view/{search_id}"
 	params = {
 		"type": type,
